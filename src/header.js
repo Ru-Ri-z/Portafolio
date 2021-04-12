f5b450eeb238f3d2b8e8aacdc1f1829084df3352
Ru-Ri-z <ivangorojovsky91gmail.com>
2021-04-12 15:48:55 -0300

    subiendolo

---
+++ src/header.js
@@ -0,0 +1,54 @@
+//gsap.to('', {duration: 5, ease: 'expo.out', y: 15});
+gsap.from('.header--button', {duration: 5, backgroundPosition: '200 0px', opacity: 0});
+gsap.from('.social-media', {duration: 15, backgroundPosition: '200 0px', opacity: 0});
+
+//gsap.from(".header--title-container", {duration:2, y:-50, opacity: 0});
+//gsap.from(".header--title-container", {duration:2, y:-50, opacity: 0, ease: "elastic(1, 0.3)"}); 
+//gsap.from("img", {duration: 1, y: 30, opacity: 0});
+gsap.from("aside", {duration: 2, opacity: 0, backgroundPosition: '200px 0px'});
+gsap.from(".interactivo", {duration:1, y:-50, opacity: 0, stagger: .6});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//gsap.from("#interactivo", {duration:1, y:-50, opacity: 0});
+
+
+ //gsap.from("#interactivo", {duration:1, y:-50, opacity: 0, ease: "elastic(1, 0.3)"}); 
+
+
+ //gsap.from("#interactivo", {duration: 3, y: -50, opacity: 0});
+ //gsap.from("#interactivo", {duration: 1, opacity: 0, backgroundPosition: '200px 0px'});
+
+// var rule = CSSRulePlugin.getRule(".header--title-container");
+                
+                // 6 Show Timeline
+  //              var tl = gsap.timeline({defaults:{duration: 3}});
+    //            tl.from(".header--title-container", {y:-50, stagger: .6, opacity: 0})
+      //            .to(rule, {duration: 1.8,cssRule: {scaleY: 0}}, "-=2.2")
+        //          .from(".header--title-container", {backgroundPosition: '200px 0px', opacity: 0}, "-=1.5")
+          //        .from(".header--title-container", {y:30, opacity: 0}, "-=.5")
